cdee1aae4d480d04f8cd55b28a729be78225656a
filtered <176114999+webfiltered@users.noreply.github.com>
2024-12-24 17:13:33 +1100

    nit

--- src/store/AppWindowSettings.ts
+++ src/store/AppWindowSettings.ts
@@ -1,8 +1,7 @@
-
 export type AppWindowSettings = {
-  windowWidth: number
-  windowHeight: number
-  windowX: number | undefined
-  windowY: number | undefined
-  windowMaximized?: boolean
-}
+  windowWidth: number;
+  windowHeight: number;
+  windowX: number | undefined;
+  windowY: number | undefined;
+  windowMaximized?: boolean;
+};
