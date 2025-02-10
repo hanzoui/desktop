8e8219e0a8a323d4f301a599a52c642d838e6997
bymyself <abolkonsky.rem@gmail.com>
2025-02-09 20:41:50 -0700

    Debounce and batch window settings writes (#867)
    * add debounce to window settings
    
    * fix maximized reset and inline debounce call
    
    * update comment
    
    * Update src/main-process/appWindow.ts
    
    Avoid spreading boolean as it results in empty set
    
    Co-authored-by: filtered <176114999+webfiltered@users.noreply.github.com>
    
    * Update src/main-process/appWindow.ts
    
    Co-authored-by: filtered <176114999+webfiltered@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: filtered <176114999+webfiltered@users.noreply.github.com>

--- src/main-process/appWindow.ts
+++ src/main-process/appWindow.ts
@@ -13,6 +13,7 @@ import {
 } from 'electron';
 import log from 'electron-log/main';
 import Store from 'electron-store';
+import { debounce } from 'lodash';
 import path from 'node:path';
 import { URL } from 'node:url';
 
@@ -286,20 +287,29 @@ export class AppWindow {
   }
 
   private setupWindowEvents(): void {
-    const updateBounds = () => {
-      if (!this.window) return;
+    const updateBounds = debounce(
+      () => {
+        if (!this.window) return;
 
-      // If maximized, do not update position / size.
-      const isMaximized = this.window.isMaximized();
-      this.store.set('windowMaximized', isMaximized);
-      if (isMaximized) return;
+        const windowMaximized = this.window.isMaximized();
+        const bounds = this.window.getBounds();
 
-      const { width, height, x, y } = this.window.getBounds();
-      this.store.set('windowWidth', width);
-      this.store.set('windowHeight', height);
-      this.store.set('windowX', x);
-      this.store.set('windowY', y);
-    };
+        // If maximized, do not update position / size, as it prevents restoring size when un-maximizing
+        const windowSizePos: Partial<AppWindowSettings> = {
+          windowWidth: bounds.width,
+          windowHeight: bounds.height,
+          windowX: bounds.x,
+          windowY: bounds.y,
+        };
+
+        this.store.set({
+          windowMaximized,
+          ...(windowMaximized ? {} : windowSizePos),
+        });
+      },
+      256,
+      { leading: true, trailing: true }
+    );
 
     this.window.on('resize', updateBounds);
     this.window.on('move', updateBounds);
