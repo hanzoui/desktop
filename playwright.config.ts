658e79c774b5c567b2f2000e91b14c4116d3ab90
filtered <176114999+webfiltered@users.noreply.github.com>
2025-02-15 15:48:32 +1100

    [e2e] Fix regression in per-test timeout (60sec) (#903)
    Resolves test fail due to timeout.

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,8 @@ export default defineConfig({
   globalSetup: './playwright.setup',
   // Entire test suite timeout - 1 hour
   globalTimeout: 60 * 60 * 1000,
+  // Per-test timeout - 60 sec
+  timeout: 60_000,
   // This is a desktop app; sharding is required to run tests in parallel.
   workers: 1,
   // GitHub reporter in CI, dot reporter for local development.
@@ -28,14 +30,10 @@ export default defineConfig({
     {
       name: 'install',
       testMatch: ['install/**/*.spec.ts', 'shared/**/*.spec.ts'],
-      // Per-test timeout - 60 sec
-      timeout: 60_000,
     },
     {
       name: 'post-install',
       testMatch: ['post-install/**/*.spec.ts', 'shared/**/*.spec.ts'],
-      // Per-test timeout - 60 sec
-      timeout: 60_000,
       dependencies: ['install'],
     },
   ],
