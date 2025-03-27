5c7b6f7df2a16a473a9f01c66a38a3843e373cc3
pythongosssss <125205205+pythongosssss@users.noreply.github.com>
2025-03-27 10:54:49 +0800

    Simplify import by using config basepath

--- src/handlers/importModelHandlers.ts
+++ src/handlers/importModelHandlers.ts
@@ -5,7 +5,7 @@ import { existsSync } from 'node:fs';
 import { copyFile, mkdir } from 'node:fs/promises';
 import path from 'node:path';
 
-import { ComfyServerConfig } from '@/config/comfyServerConfig';
+import { useDesktopConfig } from '@/store/desktopConfig';
 
 import { IPC_CHANNELS } from '../constants';
 
@@ -14,32 +14,13 @@ export function registerImportModelHandlers() {
     IPC_CHANNELS.IMPORT_MODEL,
     async (_, filePath: string, type: string, mode: 'move' | 'copy'): Promise<void> => {
       try {
-        const configPath = ComfyServerConfig.configPath;
-        const config = await ComfyServerConfig.readConfigFile(configPath);
-        if (!config) {
-          throw new Error('Unable to read extra_model_paths.yaml');
+        const basePath = useDesktopConfig().get('basePath');
+
+        if (!basePath) {
+          throw new Error('Base path is not set');
         }
 
-        // Find all config sections that have the type defined.
-        let sections = Object.entries(config)
-          .filter((c) => c[1][type])
-          .sort((a, b) => +a[1].is_default - +b[1].is_default)
-          .map((c) => c[0]);
-
-        // If no config sections with the type defined, use either the default or the first config section.
-        if (!sections.length) {
-          const defaultSection = Object.entries(config).find((c) => c[1].is_default)?.[0] ?? Object.keys(config)[0];
-          sections = [defaultSection];
-        }
-
-        const targetConfig = config[sections[0]];
-        let basePath = targetConfig.base_path;
-        if (targetConfig.download_model_base) {
-          basePath = path.join(basePath, targetConfig.download_model_base);
-        }
-
-        const folderName = targetConfig[type] ?? type;
-        const destinationDir = path.join(basePath, folderName);
+        const destinationDir = path.join(basePath, 'models', type);
         const destinationPath = path.join(destinationDir, path.basename(filePath));
 
         if (!existsSync(destinationDir)) {
