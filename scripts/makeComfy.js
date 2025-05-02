41d1af9d857d1ee13b2e313f89e320b9ad7ec37f
filtered <176114999+webfiltered@users.noreply.github.com>
2025-05-02 13:20:30 +1000

    Replace git clone script with CI action

--- scripts/makeComfy.js
+++ scripts/makeComfy.js
@@ -2,19 +2,19 @@ import { execSync } from 'node:child_process';
 
 import pkg from './getPackage.js';
 
-const comfyRepo = 'https://github.com/Comfy-Org/ComfyUI-private';
+// const comfyRepo = 'https://github.com/Comfy-Org/ComfyUI-private';
 const managerRepo = 'https://github.com/ltdrdata/ComfyUI-Manager';
 
 /** Suppress warning about detached head */
 const noWarning = '-c advice.detachedHead=false';
 
-if (pkg.config.comfyUI.optionalBranch) {
-  // Checkout branch.
-  execAndLog(`git clone ${comfyRepo} --depth 1 --branch ${pkg.config.comfyUI.optionalBranch} assets/ComfyUI`);
-} else {
-  // Checkout tag as branch.
-  execAndLog(`git ${noWarning} clone ${comfyRepo} --depth 1 --branch v${pkg.config.comfyUI.version} assets/ComfyUI`);
-}
+// if (pkg.config.comfyUI.optionalBranch) {
+//   // Checkout branch.
+//   execAndLog(`git clone ${comfyRepo} --depth 1 --branch ${pkg.config.comfyUI.optionalBranch} assets/ComfyUI`);
+// } else {
+//   // Checkout tag as branch.
+//   execAndLog(`git ${noWarning} clone ${comfyRepo} --depth 1 --branch v${pkg.config.comfyUI.version} assets/ComfyUI`);
+// }
 execAndLog(`git clone ${managerRepo} assets/ComfyUI/custom_nodes/ComfyUI-Manager`);
 execAndLog(
   `cd assets/ComfyUI/custom_nodes/ComfyUI-Manager && git ${noWarning} checkout ${pkg.config.managerCommit} && cd ../../..`
