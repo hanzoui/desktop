4fb28f05526cdbf91dfe7137f361363436e14e6e
filtered <176114999+webfiltered@users.noreply.github.com>
2025-08-14 03:30:01 +1000

    Add VC++ to installer - first pass

--- scripts/makeComfy.js
+++ scripts/makeComfy.js
@@ -21,6 +21,7 @@ execAndLog(
 );
 execAndLog(`yarn run make:frontend`);
 execAndLog(`yarn run download:uv all`);
+execAndLog(`yarn run download:vcredist`);
 execAndLog(`yarn run patch:core:frontend`);
 /**
  * Run a command and log the output.
